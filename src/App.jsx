746ab909c3092b599b286cbb90f2aa1551bff89f
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-15 00:35:16 +0330

    feat: :sparkles: Added city suggestion functionality based on user input
    I have added the city suggestion feature.
    The `App` component now uses a state to track user input and provides a hint for the closest matching city name.
    The `handleChange` function checks the user input and searches for a city name that starts with the entered characters.
    The `Input` component displays the suggested city as a hint above the input field.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 import Input from "./main";
+import cities from "./cities.json";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [hint, setHint] = useState("");
+
+  const handleChange = (e) => {
+    const inputValueTrimmed = e.target.value;
+
+    setInputValue(inputValueTrimmed);
+    setHint(inputValueTrimmed ? cities.find((city) => city.startsWith(inputValueTrimmed)) : "");
+  };
+
   return (
     <div>
-      <Input />
+      <Input handleChange={handleChange} hint={hint} inputValue={inputValue} />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 /* eslint-disable react/prop-types */
-const Input = ({handleChange, hint}) => {
+const Input = ({ handleChange, hint, inputValue }) => {
+  console.log(inputValue);
   return (
     <div className="input">
-      <label htmlFor="input" >
-        {hint}
-      </label>
-      <input
-        type="text"
-        id="input"
-        onChange={handleChange}
-      />
+      <label htmlFor="input">{hint}</label>
+      <input type="text" id="input" onChange={handleChange} />
     </div>
   );
 };
